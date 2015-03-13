1e7a144d8dbd5d282b4e9c24012af150ed5940b7
Michelle Vo <mtvo@berkeley.edu>
2015-03-13 12:03:21 -0700

    midterm1 done

--- midterm1/midterm1.r
+++ midterm1/midterm1.r
@@ -1,14 +1,17 @@
 # Please load in the dataset included in the midterm1 directory. It will be
 # required to perform the following tasks. The dataset includes data for houses
 # in the city of Berkeley.
-
+load("SFHousing-2.rda")
 
 # calculate the mean and median bsqft of houses in Berkeley. Store these as the
 # variables <mean.bsqft> and <med.bsqft> respectively.
 
 # mean.bsft <- your code here
+berkeley <- housing[housing$city == "Berkeley",]
+mean.bsqft <- mean(berkeley$bsqft, na.rm = TRUE)
+  
 # med.bsqft <- your code here
-
+med.bsqft <- median(berkeley$bsqft, na.rm = TRUE)
 
 # For each house in the dataset, calculate the squared difference between its
 # bsqft and the median bsqft of houses in Berkeley. Store this as the variable
@@ -16,8 +19,7 @@
 # the number of observations in the dataset
 
 # bsqft.diffs <- your code here
-
-
+bsqft.diffs <- (berkeley$bsqft-med.bsqft)^2
 
 # Please create two new data frames with the following two subsets
 # and store them with the indicated names:
@@ -25,16 +27,18 @@
 # 2) houses whose bsqft is less than or equal to  <mean.bsqft>: <bsqft.less>
 
 # bsqft.greater <- your code here
+bsqft.greater <- berkeley[berkeley$bsqft > mean.bsqft,]
 # bsqft.less <- your code here
+bsqft.less <- berkeley[berkeley$bsqft <= mean.bsqft,]
 
 
 # For each of your subsets, create a vector giving the price of each house. Name
 # these variables <rooms.greater.price> and <rooms.less.price>.
 
 # rooms.greater.price <- your code here
+rooms.greater.price <- bsqft.greater$price
 # rooms.less.price <- your code here
-
-
+rooms.less.price <- bsqft.less$price
 
 # Please implement the function priceByRooms. Your function should take the
 # following arguments:
@@ -49,15 +53,15 @@
 # <br> in the range (inclusive) specified by <room.range>
 
 priceByRooms <- function(room.range, br, prices) {
-
-    # your code here
+    houses <- data.frame(br,prices)
+    houses_short <- houses[houses$br >= min(room.range) & houses$br <= max(room.range),]
+    mean(houses_short$prices)
 
 }
-
 
 # Please create a plot of house price (y-axis) against br (x-axis). Your plot
 # should include the following features:
 # 1) a title "Housing price vs Number of Rooms"
 # 2) axis labels: "price" and "number of rooms"
 # 3) plotting character set to 20
-
+plot(berkeley$br, berkeley$price, xlab = "price", ylab = "number of rooms", main= "Housing price vs Number of Rooms", pch= 20)
