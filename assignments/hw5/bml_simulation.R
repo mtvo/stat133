e2afdf62f60a6615063cfb950812d04c82c4bd83
Michelle Vo <mtvo@berkeley.edu>
2015-03-14 01:56:24 -0700

    bml simulation in progress

--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -7,116 +7,40 @@
 #### The output can e.g. be how many steps the system took until it hit gridlock or
 #### how many steps you observered before concluding that it is in a free flowing state.
 
-bml.init <- function(r,c,p) {
-  car <- rep(c(1,2), ceiling((r*c*p)/2))
-  no_car <- rep(0, ceiling(r*c*(1-p)))
-  system <- sample(c(car,no_car), r*c, replace= FALSE)
-  m <- matrix(system,r,c)
+Use your BML experiment to conduct a simulation study of the behavior of the BML model.
+Below are some questions to answering. Put the code you use to run the simulation study
+in the file bml simulation.R.
+
+#Question1: For what values of p, the density of the grid, did you find free flowing traffic 
+#and traffic jams? Did you find any cases of a mixture of jams and free fowing traffic?
+
+#get image of traffic system at n-th timestep for the original matrix,m
+traffic_n <- function(m,n){
+  for (i in 1:n) {
+  m <- bml.step(m)[[1]]
+  }
   return(m)
 }
 
-blue_move <- function(m) {
-  blue.in <- m
-  for (i in 1:nrow(m)) {
-    if ((m[i,(ncol(m)-1)]==2) & (m[i,ncol(m)] != 1)) {
-      blue.in[i,ncol(m)] <-  2
-      blue.in[i,(ncol(m)-1)] <- 0
-    }
-  }
-  
-  blue.in2 <- blue.in
-  for (i in 1:nrow(blue.in2)) {
-    for (j in (ncol(blue.in2)-2):1) {
-      if ((blue.in2[i,j]== 2) & (blue.in2[i,j+1] == 0)) {
-        blue.in2[i,j+1] <- 2 
-        blue.in2[i,j] <- 0
-      } 
-    }          
-  }
-  blue.edge <- blue.in2
-  blue.edge2 <- blue.in2
-  for (i in 1:nrow(blue.in2)) {
-    if ((blue.in2[i,ncol(blue.in2)]== 2 ) & (m[i, (ncol(m)-1)]== 2) & (m[i, (ncol(m))]== 2) & (blue.in2[i,1]== 0)) {
-      blue.edge[i,1] <-2
-      blue.edge2[i,1] <- 2
-    } 
-    else if ((blue.in2[i,ncol(blue.in2)]== 2 ) & (m[i, ncol(m)]== 2) & (m[i, (ncol(m)-1)] != 2) & (blue.in2[i,1]== 0)) {
-      blue.edge[i,1] <-2
-      blue.edge[i,ncol(blue.edge)] <- 0
-      blue.edge2[i,1] <- 2
-      blue.edge2[i,ncol(blue.edge)] <- 0
-    } 
-    else if ((blue.in2[i,ncol(blue.in2)]== 2) & (m[i, ncol(m)-1]== 2) & (m[i, ncol(m)]== 2) & (blue.in2[i,1] != 0) & (blue.in2[i,ncol(blue.in2)-1]==0)) {
-      blue.edge2[i,ncol(blue.in2)-1] <- 2
-    } else if ((blue.in2[i,ncol(blue.in2)]== 2) & (m[i, ncol(m)-1]== 2) & (m[i, ncol(m)]== 2) & (blue.in2[i,1] != 0) & (blue.in2[i,ncol(blue.in2)-1] !=0)) {
-      for (j in ncol(blue.in2):2) {
-        if (blue.in2[i,j]==0) break}
-      blue.edge2[i,j] <- 2
-      
-    }
-  }
-  return(blue.edge2)
-}
+image(m, col= c("white", "red", "blue"))
 
-red_move <- function(m) {
-  red.in <- m
-  for (j in 1:ncol(m)) {
-    if ((m[(nrow(m)-1),j]==1) & (m[nrow(m),j] != 2)) {
-      red.in[nrow(m),j] <-  1
-      red.in[(nrow(m)-1),j] <- 0
-    }
-  }
-  
-  red.in2 <- red.in
-  for (j in 1:ncol(red.in2)) {
-    for (i in (nrow(red.in2)-2):1) {
-      if ((red.in2[i,j]== 1) & (red.in2[i+1,j] == 0)) {
-        red.in2[i+1,j] <- 1 
-        red.in2[i,j] <- 0
-      } 
-    }          
-  }
-  red.edge <- red.in2
-  red.edge2 <- red.in2
-  for (j in 1:ncol(red.in2)) {
-    if ((red.in2[nrow(red.in2),j]== 1 ) & (m[(nrow(m)-1),j]== 1) & (m[(nrow(m)),j]== 1) & red.in2[1,j]== 0) {
-      red.edge[1,j] <-1
-      red.edge2[1,j] <- 1
-    } 
-    else if ((red.in2[nrow(red.in2),j]== 1 ) & (m[nrow(m),j]== 1) & (m[(nrow(m)-1),j] != 1) & red.in2[1,j]== 0) {
-      red.edge[1,j] <-1
-      red.edge[nrow(red.edge),j] <- 0
-      red.edge2[1,j] <- 1
-      red.edge2[nrow(red.edge),j] <- 0
-    } 
-    else if ((red.in2[nrow(red.in2),j]== 1) & (m[nrow(m)-1,j]== 1) & (m[nrow(m),j]== 1) & (red.in2[1,j] != 0) & (red.in2[nrow(red.in2)-1,j]==0)) {
-      red.edge2[nrow(red.in2)-1,j] <- 1
-    } else if ((red.in2[nrow(red.in2),j]== 1) & (m[nrow(m)-1,j]== 1) & (m[nrow(m),j]== 1) & (red.in2[1,j] != 0) & (red.in2[nrow(red.in2)-1,j] !=0)) {
-      for (i in nrow(red.in2):2) {
-        if (red.in2[i,j]==0) break}
-      red.edge2[i,j] <- 1
-    }
-  }
-  return(red.edge2)
-}
+#Question2: How many simulation steps did you need to run before observing this behavior?
 
-bml.step <- function(m){
-  orig.m <-m
-  grid.new <- logical()
-  m <- blue_move(m)
-  m <- red_move(m)
-  grid.new <- sum(orig.m != m) > 0
-  return(list(m,grid.new))
-}
+#Question3: Does the transition depend on the size or shape of the grid?
+bml.sim(200,200,0.30)
+#[1] 598
+ 
+bml.sim(100,100,0.30)
+#[1] 349
 
-bml.sim <- function(r,c,p) {
-  m <- bml.init(r,c,p)
-  i <- 1
-  while (i <= 20000) {
-    i <- i + 1
-    new.m <- bml.step(m)
-    m <- new.m[[1]]
-    y <- new.m[[2]]
-    if (y== FALSE) break}
-  print(i-1)
-}
+bml.sim(10,10,0.30)
+#[1] 1000
+
+
+To answer these questions, run the experiment for different values of the input parameters.
+For each set of input parameters that you examine, run the experiment multiple times to
+understand the variability in the process. For example, does a p = 0:8 always end in a
+traffic jam or does it only end in a traffic jam 70% of the time? Answer these questions
+in the text file bml comments.txt or upload a report named bml comments.pdf. You
+are encouraged to include 1-4 plots to support your observations, either as a separate file
+(bml figures.pdf) or embedded in the pdf file with your comments.
